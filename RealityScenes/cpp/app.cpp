4684be20c082811f094d8e44b1fb390fc4941522
Eric Yoon <emjyoon@gmail.com>
2025-04-15 18:52:23 -0400

    Project building for Android (albeit not in VR)

--- RealityScenes/cpp/app.cpp
+++ RealityScenes/cpp/app.cpp
@@ -9,9 +9,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if (!SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Reality Scenes", "Hello world!", NULL)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_ShowSimpleMessageBox failed (%s)", SDL_GetError());
-        return 1;
+    SDL_Window* window = SDL_CreateWindow("Rerality Scenes", 960, 540, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    bool quitOnNextFrame = false;
+    while(!quitOnNextFrame) {
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quitOnNextFrame = true;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
     }
 
     SDL_Quit();
